3289f2bf512f33529bfb54de1aae4b9aa48d23c6
OfficePC <WenanLee@outlook.com>
2021-05-11 18:52:16 +0800

    add commandTerminal

---
+++ CommandTerminal/Test/Car.cs
@@ -0,0 +1,39 @@
+﻿using CommandTerminal;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    void Start()
+    {
+        Terminal.Shell.AddCommand("test", Test, 0, 1, "测试非静态", "提示");
+        //自动补全
+        Terminal.Autocomplete.Register("test");
+    }
+    [RegisterCommand(Help = "开车", Hint = "car 0/1/2/3", MaxArgCount = 1, MinArgCount = 1, Name = "set.car")]
+    public static void Run(CommandArg[] args)
+    {
+        switch (args[0].Int)
+        {
+            case 0:
+                Debug.Log("前");
+                break;
+            case 1:
+                Debug.Log("后");
+                break;
+            case 2:
+                Debug.Log("左");
+                break;
+            case 3:
+                Debug.Log("右");
+                break;
+            default:
+                break;
+        }
+    }
+    public void Test(CommandArg[] args)
+    {
+        Terminal.Log(name);
+    }
+}
---
+++ CommandTerminal/Test/Pad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            CommandTerminal.Terminal.Shell.RunCommand("car 1");
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            foreach (var item in CommandTerminal.Terminal.Shell.Commands)
+            {
+                CommandTerminal.Terminal.Log(item.Key);
+            }
+        }
+        //help 
+        //help car (输出"Hint")
+    }
+}
