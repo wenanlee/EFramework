e912ac83a88858774e36d142a9f609c8e66b1d48
OfficePC <WenanLee@outlook.com>
2021-05-11 20:13:32 +0800

    添加测试

--- Demo/Event/EventsDemo.cs
+++ Demo/Event/EventsDemo.cs
@@ -12,12 +12,12 @@ public class EventsDemo : MonoBehaviour
     {
         Login += Loginn;
         Login.InvokeSafe("wenan", "123321123");
-
-       
+        EventManager.AddListener<string, string>(1, Loginn);
+        EventManager.SendEvent(1,"wenan1","456654456");
     }
-    private void Loginn(string name,string password)
+    private void Loginn(string name, string password)
     {
-        Debug.Log(name+"   "+password);
+        Debug.Log(name + "   " + password);
         TweeningRotate.Begin(gameObject, Vector3.zero, Vector3.up * 360, 10);
     }
 }
