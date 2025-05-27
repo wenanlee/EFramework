3e11edf654dee6f16eecbf6406137c6a5a9bdc92
WENANLEEPC\WenanLee <WenanLee@outlook.com>
2025-05-27 10:37:28 +0800

    添加 NodeGraphProcessor

---
+++ EFramework/UnityEFramework/3rdPlugins/NodeGraphProcessor/Runtime/Processing/ProcessGraphProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Unity.Jobs;
+using Unity.Collections;
+// using Unity.Entities;
+
+namespace GraphProcessor
+{
+
+	/// <summary>
+	/// Graph processor
+	/// </summary>
+	public class ProcessGraphProcessor : BaseGraphProcessor
+	{
+		List< BaseNode >		processList;
+		
+		/// <summary>
+		/// Manage graph scheduling and processing
+		/// </summary>
+		/// <param name="graph">Graph to be processed</param>
+		public ProcessGraphProcessor(BaseGraph graph) : base(graph) {}
+
+		public override void UpdateComputeOrder()
+		{
+			processList = graph.nodes.OrderBy(n => n.computeOrder).ToList();
+		}
+
+		/// <summary>
+		/// Process all the nodes following the compute order.
+		/// </summary>
+		public override void Run()
+		{
+			int count = processList.Count;
+
+			for (int i = 0; i < count; i++)
+				processList[i].OnProcess();
+		}
+	}
+}
