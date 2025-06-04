09dbb3720ae21d83046a691985126819b6017e43
WENANLEEPC\WenanLee <WenanLee@outlook.com>
2025-06-04 17:54:39 +0800

    给定时添加loop参数

--- EFramework/UnityEFramework/3rdPlugins/UrFairy-master/MonoBehaviourExtensions.cs
+++ EFramework/UnityEFramework/3rdPlugins/UrFairy-master/MonoBehaviourExtensions.cs
@@ -4,34 +4,43 @@ using System.Collections;
 
 public static class MonoBehaviourExtensions
 {
-    public static void Delay(this MonoBehaviour g, Action f)
+    public static void Delay(this MonoBehaviour g, Action f, bool loop = false)
     {
-        Delay(g, 1, f);
+        Delay(g, 1, f, loop);
     }
 
-    public static void Delay(this MonoBehaviour g, int frames, Action f)
+    public static void Delay(this MonoBehaviour g, int frames, Action f, bool loop = false)
     {
-        g.StartCoroutine(DelayCoroutine(frames, f));
+        g.StartCoroutine(DelayCoroutine(frames, f, loop));
     }
 
-    public static void Delay(this MonoBehaviour g, float seconds, Action f)
+    public static void Delay(this MonoBehaviour g, float seconds, Action f, bool loop = false)
     {
-        g.StartCoroutine(DelayCoroutine(seconds, f));
+        g.StartCoroutine(DelayCoroutine(seconds, f, loop));
     }
 
-    public static IEnumerator DelayCoroutine(int frames, Action f)
+    public static IEnumerator DelayCoroutine(int frames, Action f, bool loop = false)
     {
-        for (var n = 0; n < frames; ++n)
+        do
         {
-            yield return null;
-        }
+            for (var n = 0; n < frames; ++n)
+            {
+                yield return null;
+            }
 
-        f();
+            f();
+        }
+        while (loop);
+
     }
 
-    public static IEnumerator DelayCoroutine(float seconds, Action f)
+    public static IEnumerator DelayCoroutine(float seconds, Action f, bool loop = false)
     {
-        yield return new WaitForSeconds(seconds);
-        f();
+        do
+        {
+            yield return new WaitForSeconds(seconds);
+            f();
+        }
+        while (loop);
     }
 }
