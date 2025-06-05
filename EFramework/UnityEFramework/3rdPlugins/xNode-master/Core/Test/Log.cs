c80ef27a49c01a9d250c06b80a35476a444e9870
WENANLEEPC\WenanLee <WenanLee@outlook.com>
2025-06-05 10:58:09 +0800

    添加测试

--- EFramework/UnityEFramework/3rdPlugins/xNode-master/Core/Test/Log.cs
+++ EFramework/UnityEFramework/3rdPlugins/xNode-master/Core/Test/Log.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using EFramework.Unity.XNode;
-[CreateNodeMenu("Log",menuName ="打印节点",nodeName ="打印节点")]
-public class Log : TaskNode {
+using EFramework.Unity.XNode.Core;
+namespace EFramework.Unity.XNode.Test
+{
+    [CreateNodeMenu("Log", menuName = "打印节点", nodeName = "打印节点")]
+    public class Log : TaskNode
+    {
 
-	[Input] // Input port for the node
-    public string message;
-    protected override void Init() {
-		base.Init();
+        [Input] // Input port for the node
+        public string message;
+        protected override void Init()
+        {
+            base.Init();
+        }
+
+        // Return the correct value of an output port when requested
+        public override object GetValue(NodePort port)
+        {
+            return null; // Replace this
+        }
     }
-
-	// Return the correct value of an output port when requested
-	public override object GetValue(NodePort port) {
-		return null; // Replace this
-	}
 }
