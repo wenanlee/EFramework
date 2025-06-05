9b88b0e40a697b72708526af31e97c083fc11b47
WENANLEEPC\WenanLee <WenanLee@outlook.com>
2025-06-05 10:58:45 +0800

    添加流程节点

---
+++ EFramework/UnityEFramework/3rdPlugins/xNode-master/Core/ProcessNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EFramework.Unity.XNode;
+using EFramework.Core;
+namespace EFramework.Unity.XNode.Core
+{
+    [CreateNodeMenu("系统/流程节点",nodeName ="流程节点")]
+    public class ProcessNode : TaskNodeBase
+    {
+        public string ProcessName = "";
+        public void StartProcess()=> EventManager.SendEvent("ProcessStart", ProcessName);
+        public void EndProcess() => EventManager.SendEvent("ProcessEnd", ProcessName);
+    }
+}
